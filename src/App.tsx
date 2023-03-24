9bcfa770a6bce6c34de3838790916db52f7c1e8d
jbv2 <j4petv2@gmail.com>
2023-03-24 16:49:51 +0900

    6.2 useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
@@ -7,12 +7,28 @@ import Button from './Button'
 import styles from './App.module.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [counter, setCounter] = useState(0)
+  const [keyword, setKeyword] = useState('')
+  function onChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setKeyword(event.target.value)
+  }
+  function onClick() {
+    setCounter((cur) => cur + 1)
+  }
+  console.log('render')
+  function isRunOnlyOnce() {
+    console.log('once')
+  }
+  useEffect(isRunOnlyOnce, [])
+  useEffect(() => {
+    if (keyword !== '') console.log('Search For', keyword)
+  }, [keyword])
   return (
     <div>
-      <h1 className={styles.title}>1test</h1>
-      <Button text='test2' cls='btn' />
+      <input onChange={onChange} type='text' placeholder='Search'></input>
+      <h1 className={styles.title}>1test {counter}</h1>
+      <button onClick={onClick}>{counter}</button>
+      {/* <Button text='test2' cls='btn' /> */}
     </div>
   )
 }
